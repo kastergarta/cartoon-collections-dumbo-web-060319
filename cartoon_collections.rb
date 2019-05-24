79b7d150dff24f4780c53d482f4feca4b99a8d24
Dennis <dennis.basyrov@gmail.com>
2019-05-24 03:29:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?(cheese_types)
+  array.find do |item|
+  item.include?(cheese_types)
+end
+end
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
